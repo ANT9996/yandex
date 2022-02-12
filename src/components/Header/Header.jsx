38d86d4df929cbfb8d5f95b4cac53c654a1b3e49
ANT9996 <bocsyr.516@gmail.com>
2022-02-13 02:58:52 +0600

    yandex

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import c from './Header.module.css';
+import logo from './../../yandex_taxi_logo_norm.svg';
+
+let Header = () => {
+    return (
+        <div className={`${c.header} pe-5 ps-5`}>
+            <div className="row">
+                <div className="col-4">
+                    <div className={c.logo_block}>
+                        <div><img src={logo} alt="логотип" /></div>
+                        <div>Официальный партнер <br></br> г.Омск</div>
+                    </div>
+                </div>
+                <div className="col-5">
+                    <div className={c.nav}>
+                        <div><a href="">Начало работы</a></div>
+                        <div><a href="">Преимущества работы</a></div>
+                        <div><a href="">Приложение</a></div>
+                        <div><a href="">Контакты</a></div>
+                    </div>
+                </div>
+                <div className="col-3">
+                    <div className={`${c.tel}`}>
+                        <div><b>8 (3812) 49-46-45</b></div>
+                        <div className={c.req}>заказать звонок</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
