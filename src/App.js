67ac3c2033424bcf2c261fe0367c571775ee729d
ANT9996 <bocsyr.516@gmail.com>
2022-04-02 16:00:18 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
-import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 import Block_1 from './components/Block_1/Block_1';
-import Block_2 from './components/Block_2/Block_1/Block_2';
+import Block_2 from './components/Block_2/Block_2';
 import Block_3 from './components/Block_3/Block_3';
 import Block_4 from './components/Block_4/Block_4';
 import Block_5 from './components/Block_5/Block_5';
