34f4c5e5684d06624032f9ca8240c78380ccd7b0
ANT9996 <bocsyr.516@gmail.com>
2023-02-21 01:03:02 +0600

    ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
 import Block1 from './components/Block1/Block1';
 import Block2 from './components/Block2/Block2';
-import Block3 from './components/Block_3/Block3';
-import Block4 from './components/Block_4/Block4';
-import Block5 from './components/Block_5/Block5';
-import Block6 from './components/Block_6/Block6';
-import Block7 from './components/Block_7/Block7';
+import Block3 from './components/Block3/Block3';
+import Block4 from './components/Block4/Block4';
+import Block5 from './components/Block5/Block5';
+import Block6 from './components/Block6/Block6';
+import Block7 from './components/Block7/Block7';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import MobileHeader from './components/MobileHeader/MobileHeader';
